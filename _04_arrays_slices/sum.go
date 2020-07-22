39b60754fb0fc27e26be5fd322cde9fce8982129
Vlads <vladsbg@gmail.com>
2020-07-22 20:36:46 -0300

    feat:(content) slices, reflect

--- _04_arrays_slices/sum.go
+++ _04_arrays_slices/sum.go
@@ -13,3 +13,16 @@ func SumAll(numbers ...[]int) (result []int) {
 	}
 	return
 }
+
+func SumAllRest(numbers ...[]int) (result []int) {
+	for _, values := range numbers {
+		if len(values) > 0 {
+			final := values[1:]
+			result = append(result, Sum(final))
+		} else {
+			result = append(result, 0)
+		}
+	}
+
+	return
+}
--- _04_arrays_slices/sum_test.go
+++ _04_arrays_slices/sum_test.go
@@ -6,6 +6,13 @@ import (
 )
 
 func TestSum(t *testing.T) {
+	checkSum := func(t *testing.T, want, got []int) {
+		t.Helper()
+		if !reflect.DeepEqual(want, got) {
+			t.Errorf(" queria '%d', obteve '%d'", want, got)
+		}
+	}
+
 	t.Run("soma coleção de n... números", func(t *testing.T) {
 
 		numbers := []int{1, 2, 3}
@@ -21,8 +28,20 @@ func TestSum(t *testing.T) {
 		want := []int{6, 9}
 		got := SumAll([]int{1, 2, 3}, []int{0, 9})
 
-		if !reflect.DeepEqual(want, got) {
-			t.Errorf(" queria '%d', obteve '%d'", want, got)
-		}
+		checkSum(t, want, got)
+	})
+
+	t.Run("soma todo o resto", func(t *testing.T) {
+		want := []int{2, 9}
+		got := SumAllRest([]int{1, 2}, []int{0, 9})
+
+		checkSum(t, want, got)
+	})
+
+	t.Run("soma slices vazios de forma segura", func(t *testing.T) {
+		want := []int{0, 9}
+		got := SumAllRest([]int{}, []int{3, 4, 5})
+
+		checkSum(t, want, got)
 	})
 }
