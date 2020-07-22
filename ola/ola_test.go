2ee0acac102220b33bd49e190af36a55dca9ed8d
Vlads <vladsbg@gmail.com>
2020-07-21 22:48:50 -0300

    refactor: code style

--- ola/ola_test.go
+++ ola/ola_test.go
@@ -6,10 +6,10 @@ import (
 
 func TestOla(t *testing.T) {
 
-	checkCorrectMessage := func(t *testing.T, result, expected string) {
+	checkCorrectMessage := func(t *testing.T, expected, result string) {
 		t.Helper() //indica que é um método auxiliar (necessário)
 		if result != expected {
-			t.Errorf("resultado '%s', esperado '%s'", result, expected)
+			t.Errorf("esperado '%s', resultado '%s'", expected, result)
 		}
 	}
 
@@ -17,34 +17,34 @@ func TestOla(t *testing.T) {
 		result := Hello("Chris", "português")
 		expected := "Olá, Chris"
 
-		checkCorrectMessage(t, result, expected)
+		checkCorrectMessage(t, expected, result)
 	})
 
 	t.Run(" diz 'Olá, mundo' quando uma string vazia for passada", func(t *testing.T) {
 		result := Hello("", "")
 		expected := "Olá, mundo"
 
-		checkCorrectMessage(t, result, expected)
+		checkCorrectMessage(t, expected, result)
 	})
 
 	t.Run(" em espanhol", func(t *testing.T) {
 		result := Hello("Elodie", "espanhol")
 		expected := "Hola, Elodie"
 
-		checkCorrectMessage(t, result, expected)
+		checkCorrectMessage(t, expected, result)
 	})
 
 	t.Run(" em francês", func(t *testing.T) {
 		result := Hello("Maria", "francês")
 		expected := "Bonjour, Maria"
 
-		checkCorrectMessage(t, result, expected)
+		checkCorrectMessage(t, expected, result)
 	})
 
 	t.Run(" em italiano", func(t *testing.T) {
 		result := Hello("Pepe", "italiano")
 		expected := "Ciao, Pepe"
 
-		checkCorrectMessage(t, result, expected)
+		checkCorrectMessage(t, expected, result)
 	})
 }
