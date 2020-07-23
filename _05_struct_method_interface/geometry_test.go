6033c9a344c01223c84f4e1373ba99b8e924adad
Vlads <vladsbg@gmail.com>
2020-07-22 21:57:00 -0300

    feat:(content)struct,interfaces,polimorfismo

--- _05_struct_method_interface/geometry_test.go
+++ _05_struct_method_interface/geometry_test.go
@@ -16,18 +16,21 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 	testsArea := []struct {
+		name string
 		form Form
 		want float64
 	}{
-		{Rectangle{12, 6}, 72.0},
-		{Circle{10}, 314.1592653589793},
+		{name: "retangulo", form: Rectangle{Height: 6, Width: 12}, want: 72.0},
+		{name: "circulo", form: Circle{Radius: 10}, want: 314.1592653589793},
+		{name: "triangulo", form: Triangle{Base: 12, Height: 6}, want: 36.0},
 	}
 
 	for _, tt := range testsArea {
-		got := tt.form.Area()
-		if tt.want != got {
-			t.Errorf(" queria '%.2f', obteve '%.2f'", tt.want, got)
-		}
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.form.Area()
+			if tt.want != got {
+				t.Errorf("%#v queria '%.2f', obteve '%.2f'", tt.name, tt.want, got)
+			}
+		})
 	}
-
 }
