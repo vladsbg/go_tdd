2d92bea415374d3f029c5c11fbba12bfa31f22a2
Vlads <vladsbg@gmail.com>
2020-07-24 21:56:25 -0300

    feat: study more

---
+++ _12_reflection/v2/reflection_test.go
@@ -0,0 +1,36 @@
+package reflection
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestWalk(t *testing.T) {
+	cases := []struct {
+		Name          string
+		Input         interface{}
+		ExpectedCalls []string
+	}{
+		{
+			"Struct com um campo string",
+			struct {
+				Name string
+				Age  int
+			}{"Chris", 33},
+			[]string{"Chris"},
+		},
+	}
+
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+			walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
+
+			if !reflect.DeepEqual(got, test.ExpectedCalls) {
+				t.Errorf("obteve '%v', esperado '%v'", got, test.ExpectedCalls)
+			}
+		})
+	}
+}
